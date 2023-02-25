dd058525ecdeb3f5d2707754563e807f66687efa
Sebben7Sebben <99653108+Sebben7Sebben@users.noreply.github.com>
2023-02-25 15:54:12 +0100

    Add files via upload

---
+++ NoteUpdate.lua
@@ -0,0 +1,10 @@
+
+function ColdEmbrace_UpdateNotes()		
+	for i = 1,250 do
+		name, rank, rankIndex, level, class, zone, note, officernote, online, status = GetGuildRosterInfo(i);
+
+																
+
+		
+	end
+end
